4e953ddf2162e50815111440745a8f03ba30ab86
onuki <k022g0020@g.neec.ac.jp>
2023-04-07 06:28:50 +0900

    プロジェクト ファイルを追加します。

---
+++ 2d/ImGuiManager.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "DirectXCommon.h"
+#include "WinApp.h"
+#include <imgui.h>
+
+class ImGuiManager {
+public:
+	static ImGuiManager* GetInstance();
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize(WinApp* winApp, DirectXCommon* dxCommon);
+
+	/// <summary>
+	/// 終了
+	/// </summary>
+	void Finalize();
+
+	/// <summary>
+	/// ImGui受付開始
+	/// </summary>
+	void Begin();
+
+	/// <summary>
+	/// ImGui受付終了
+	/// </summary>
+	void End();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private:
+	// DirectX基盤インスタンス（借りてくる）
+	DirectXCommon* dxCommon_ = nullptr;
+	// SRV用ヒープ
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> srvHeap_;
+
+private:
+	ImGuiManager() = default;
+	~ImGuiManager() = default;
+	ImGuiManager(const ImGuiManager&) = delete;
+	const ImGuiManager& operator=(const ImGuiManager&) = delete;
+};
---
+++ 3d/ViewProjection.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "Matrix4x4.h"
+#include "Vector3.h"
+#include <d3d12.h>
+#include <wrl.h>
+
+// 定数バッファ用データ構造体
+struct ConstBufferDataViewProjection {
+	Matrix4x4 view;       // ワールド → ビュー変換行列
+	Matrix4x4 projection; // ビュー → プロジェクション変換行列
+	Vector3 cameraPos;    // カメラ座標（ワールド座標）
+};
+
+/// <summary>
+/// ビュープロジェクション変換データ
+/// </summary>
+struct ViewProjection {
+	// 定数バッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> constBuff_;
+	// マッピング済みアドレス
+	ConstBufferDataViewProjection* constMap = nullptr;
+
+#pragma region ビュー行列の設定
+	// X,Y,Z軸回りのローカル回転角
+	Vector3 rotation_ = {0, 0, 0};
+	// ローカル座標
+	Vector3 translation_ = {0, 0, -50};
+#pragma endregion
+
+#pragma region 射影行列の設定
+	// 垂直方向視野角
+	float fovAngleY = 45.0f * 3.141592654f / 180.0f;
+	// ビューポートのアスペクト比
+	float aspectRatio = (float)16 / 9;
+	// 深度限界（手前側）
+	float nearZ = 0.1f;
+	// 深度限界（奥側）
+	float farZ = 1000.0f;
+#pragma endregion
+
+	// ビュー行列
+	Matrix4x4 matView;
+	// 射影行列
+	Matrix4x4 matProjection;
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+	/// <summary>
+	/// 定数バッファ生成
+	/// </summary>
+	void CreateConstBuffer();
+	/// <summary>
+	/// マッピングする
+	/// </summary>
+	void Map();
+	/// <summary>
+	/// 行列を更新する
+	/// </summary>
+	void UpdateMatrix();
+	/// <summary>
+	/// 行列を転送する
+	/// </summary>
+	void TransferMatrix();
+	/// <summary>
+	/// ビュー行列を更新する
+	/// </summary>
+	void UpdateViewMatrix();
+	/// <summary>
+	/// 射影行列を更新する
+	/// </summary>
+	void UpdateProjectionMatrix();
+};
