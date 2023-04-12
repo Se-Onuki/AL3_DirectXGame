828500ac092f8251fcb64cf78f29cf470560ac88
onuki <k022g0020@g.neec.ac.jp>
2023-04-12 19:09:00 +0900

    コピー2回目

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,6 +1,7 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include <imgui.h>
 
 GameScene::GameScene() {}
 
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -9,6 +9,9 @@
 #include "ViewProjection.h"
 #include "WorldTransform.h"
 
+#include "DebugCamera.h"
+
+
 /// <summary>
 /// ゲームシーン
 /// </summary>
@@ -46,12 +49,18 @@ private: // メンバ変数
 	Audio* audio_ = nullptr;
 
 	uint32_t textureHandle_ = 0;
+	Sprite* sprite_ = nullptr;
 	Model* model_ = nullptr;
 
 	WorldTransform worldTransform_;
 	ViewProjection viewProjection_;
 
-	Audio* audio_ = nullptr;
+	uint32_t soundDataHandle_ = 0;
+	uint32_t voiceHandle_ = 0;
+
+	float inputFloat3[3] = {0, 0, 0};
+
+	DebugCamera* debugCamera_ = nullptr;
 
 	/// <summary>
 	/// ゲームシーン用
